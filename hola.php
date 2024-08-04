545ff7a2ee2aa95ec7c2de06ae0a6246482f4f77
milenacarola <milenacarola19@gmail.com>
2024-08-04 17:42:25 -0400

    Mi primer commit

---
+++ hola.php
@@ -0,0 +1,16 @@
+<?php
+$semana = array('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo' );
+
+//$auxiliar = array_pop($semana);
+//foreach($semana as $dia){
+  //  echo $dia .' </br>';
+
+//}
+
+echo join (' - ', $semana);
+
+
+?>
+
+    
+ 
